8171e8cf476a42d86d73ae56030aba7c39265b71
Poisk <vpisotski@gmail.com>
2020-06-05 16:23:53 -0700

    added pictures to goals using Visibility Sensor

--- client/src/js/components/mainPage/mainPageComponents/Goals.jsx
+++ client/src/js/components/mainPage/mainPageComponents/Goals.jsx
@@ -3,9 +3,10 @@ import VizAwareImg from './VizAwareImage';
 
 function Goals(props) {
   return (
-
     <div>
-      <VizAwareImg scr="https://media.wired.com/photos/5a593a7ff11e325008172bc2/master/w_2560%2Cc_limit/pulsar-831502910.jpg" />
+      <p>
+        <VizAwareImg src="https://media.wired.com/photos/5a593a7ff11e325008172bc2/master/w_2560%2Cc_limit/pulsar-831502910.jpg" />
+      </p>
     </div>
   );
 }
--- client/src/js/components/mainPage/mainPageComponents/VizAwareImage.jsx
+++ client/src/js/components/mainPage/mainPageComponents/VizAwareImage.jsx
@@ -9,22 +9,26 @@ class VizAwareImg extends React.Component {
   }
 
   render() {
+    const { src } = this.props;
+
     return (
-      <VizSensor>
-        {/* onChange=
-        {(isVisible) => {
+      <VizSensor
+        // partialVisibility
+        offset={{ bottom: -150 }}
+        onChange={(isVisible) => {
           this.setState({ imgViz: isVisible });
         }}
       >
         <img
-          src={this.props.src}
+          src={src}
           style={{
+            zIndex: 1,
             width: 300,
             height: 300,
             opacity: this.state.imgViz ? 1 : 0.25,
             transition: 'opacity 500ms linear',
           }}
-        /> */}
+        />
       </VizSensor>
     );
   }
